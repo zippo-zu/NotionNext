8688a7c25e888d13a4d07a12db9629ec72a07aa4
黄大仙爱代码爱篮球 <66836597+zippo-zu@users.noreply.github.com>
2024-09-11 17:27:12 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -28,15 +28,8 @@ const Footer = ({ title }) => {
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left'>
-           {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
-          {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .
-           本站作为一个独立的个人笔记订阅平台，不生产和发布任何内容， 所有笔记均源于作者本人编写，本站不存储任何数据，禁止发布涉及黄赌毒和违背道德和法律的笔记。 如果本站用户发布内容侵犯了您的权益，请及时联系我们或作者本人。我们将第一时间进行处理！联系邮箱：zuzippo@gmail.com                  
+          
+ 本站作为一个独立的个人笔记订阅平台，不生产和发布任何内容， 所有笔记均源于作者本人编写，本站不存储任何数据，禁止发布涉及黄赌毒和违背道德和法律的笔记。 如果本站用户发布内容侵犯了您的权益，请及时联系我们或作者本人。我们将第一时间进行处理！联系邮箱：zuzippo@gmail.com                  
         </div>
 
         <div id='footer-bottom-right'>
